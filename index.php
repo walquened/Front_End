3f607074e624949a952817c8230a62bcb98930f4
kenin <timidia3@gmail.com>
2019-04-06 21:54:12 -0300

    Front-End / Adcionando Efeitos Animated.css com JavaScript

--- index.php
+++ index.php
@@ -1,176 +1,176 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <title> BykMin 2019yyy </title>
-        <meta name="description" content="Acesse as Informações Com Facilidade Agilidade em Tempo Real ">
-        <meta property="og:type" content="website"/>
-        <meta property="og:title" content="BykMin"/>
-        <meta property="og:description" content="Acesse as Informações Com Facilidade Agilidade em Tempo Real "/>
-        <meta property="og:url" content="http://www.timidia.com.br/doc/BykMin"/>
-        <!--<meta property="og:image" content="http://www.timidia.com.br/img/banner.jpg"/>--> <!--link com a imagem  do site-->
+ <head>
+  <meta charset="UTF-8">
+  <title> BykMin 2019yyy </title>
+  <meta name="description" content="TiMidia - Gerenciamento de Beckups em Nuvens ">
+  <meta property="og:type" content="website"/>
+  <meta property="og:title" content="TiMídia"/>
+  <meta property="og:description" content="Gerencie Seus Backups Automático "/>
+  <meta property="og:url" content="http://www.timidia.com/"/>
+  <!--<meta property="og:image" content="http://www.timidia.com.br/img/banner.jpg"/> link com a imagem  do site-->
 
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="shortcut icon" href="nomedoicom" />
-        <link rel="stylesheet" href="css/normalize.css"/>
-        <link rel="stylesheet" href="css/reset.css"/>
-        <link rel="stylesheet" href="css/grid.css"/>
-        <link rel="stylesheet" href="css/style.css"/>
-        <link rel="stylesheet" href="css/responsivo.css"/>
-    </head>
-    <script>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="nomedoicom" />
+  <link rel="stylesheet" href="css/normalize.css"/>
+  <link rel="stylesheet" href="css/reset.css"/>
+  <link rel="stylesheet" href="css/grid.css"/>
+  <link rel="stylesheet" href="css/style.css"/>
+  <link rel="stylesheet" href="css/responsivo.css"/>
+ </head>
+ <script>
 
-    </script>
-    <body>
-        <?php
-        // Digite o Codigo PHP aqui!ddddd
-        ?>
-        <header class="header">
-            <h1 class="fontzero">BykMin</h1>
-            <div class="container">
-                <a href="index.php" class="grid-4"> 
-                    <img src="img/logo-topo.png"> </a>
+ </script>
+ <body>
+  <?php
+  // Digite o Codigo PHP aqui!ddddd
+  ?>
+  <header class="header">
+   <h1 class="fontzero">BykMin</h1>
+   <div class="container">
+    <a href="index.php" class="grid-4"> 
+     <img src="img/logo-topo.png"> </a>
 
-                <nav class="grid-12 header_menu">
-                    <h6 class="fontzero">BykMin</h6>
-                    <ul>
-                        <li><a href="sobre.php" title="Sobre">Sobre</a></li> 
-                        <li><a href="produtos.php" title="Produtos">Produtos</a></li> 
-                        <li><a href="portfolio.php" title="Portfólio">Portfólio</a></li> 
-                        <li><a href="contato.php" title="Contato">Contato</a></li> 
-                    </ul>
-                </nav>
-            </div>
-        </header>
-        <!--FIM HEADER -->
-        <section class="introducao">
-            <div class="container">
-                <h1>Bicicletas Feitas a Mãos</h1>
-                <blockquote class="quote-externo"><p>" Não tenha nada em sua cassa que você não considere útil ou acredita ser bonito"</p>
-                    <cite>william morris</cite></blockquote>
-                <a href="produtos.php" class="btn">Orçamentos</a>
-            </div>
+    <nav class="grid-12 header_menu">
+     <h6 class="fontzero">BykMin</h6>
+     <ul>
+      <li><a href="sobre.php" title="Sobre">Sobre</a></li> 
+      <li><a href="produtos.php" title="Produtos">Produtos</a></li> 
+      <li><a href="portfolio.php" title="Portfólio">Portfólio</a></li> 
+      <li><a href="contato.php" title="Contato">Contato</a></li> 
+     </ul>
+    </nav>
+   </div>
+  </header>
+  <!--FIM HEADER -->
+  <section class="introducao">
+   <div class="container">
+    <h1>Bicicletas Feitas a Mãos</h1>
+    <blockquote class="quote-externo"><p>" Não tenha nada em sua cassa que você não considere útil ou acredita ser bonito"</p>
+     <cite>william morris</cite></blockquote>
+    <a href="produtos.php" class="btn">Orçamentos</a>
+   </div>
 
-        </section>
-        <!--FIM INTRODUÇÃO -->
-        <section class="produtos container"> 
-            <h2 class="subtitulo">Produtos</h2>
-            <ul class="produtos_lista">
-                <li class="grid-1-3">
-                    <div class="produtos_icone"> 
-                        <img src="./img/produtos/passeio.png" alt="[BykMin Passeio]" title="TiMídia Passeio"></div>
-                    <h3>Passeio</h3>
-                    <p> Não tenha nada em sua cassa que você não considere útil ou acredita ser bonito</p>
-                </li>
+  </section>
+  <!--FIM INTRODUÇÃO -->
+  <section class="produtos container animar"> 
+   <h2 class="subtitulo">Produtos</h2>
+   <ul class="produtos_lista">
+    <li class="grid-1-3">
+     <div class="produtos_icone"> 
+      <img src="./img/produtos/passeio.png" alt="[BykMin Passeio]" title="TiMídia Passeio"></div>
+     <h3>Passeio</h3>
+     <p> Não tenha nada em sua cassa que você não considere útil ou acredita ser bonito</p>
+    </li>
 
-                <li class="grid-1-3">
-                    <div class="produtos_icone"> 
-                        <img src="./img/produtos/esporte.png" alt="[BykMin Passeio]" title="TiMídia Passeio"></div>
-                    <h3>Esporte</h3>
-                    <p> Não tenha nada em sua cassa que você não considere útil ou acredita ser bonito</p>
-                </li>
+    <li class="grid-1-3">
+     <div class="produtos_icone"> 
+      <img src="./img/produtos/esporte.png" alt="[BykMin Passeio]" title="TiMídia Passeio"></div>
+     <h3>Esporte</h3>
+     <p> Não tenha nada em sua cassa que você não considere útil ou acredita ser bonito</p>
+    </li>
 
-                <li class="grid-1-3">
-                    <div class="produtos_icone"> 
-                        <img src="./img/produtos/retro.png" alt="[BykMin Passeio]" title="TiMídia Passeio"></div>
-                    <h3>Retro</h3>
-                    <p> Não tenha nada em sua cassa que você não considere útil ou acredita ser bonito</p>
-                </li>
-            </ul>
-            <div class="call">
-                <p>Clique Aqui e Veja os Detalhes dos Produtos</p>
-                <a href="Produtos.php" class="btn btn-preto">Produtos</a>
-            </div>
-        </section>
-        <!-- FECHA PRODUTOS --> 
-        <section class="portfolio">
-            <div class="container">
-                <h2 class="subtitulo">Portfólio</h2>
-                <div class="portfolio_lista">
-                    <div class="grid-8"><img src="img/portfolio/retro.jpg" alt="[Bicicleta Retro]"></div>
-                    <div class="grid-8"><img src="img/portfolio/passeio.jpg" alt="[Bicicleta Passeio]"></div> 
-                    <div class="grid-16"><img src="img/portfolio/esporte.jpg" alt="[Bicicleta Esporte]"></div>
-                </div>
-                <div class="call">
-                    <p>Conheça Mais Sobre Nosso Portfólio</p>
-                    <a href="portfolio.php" class="btn">Portfólio</a>
-                </div>
-            </div>            
-        </section>            <!-- FIM PORTFÓLIO -->
-        <section class="qualidade container">
-            <h2 class="subtitulo">Qualidade</h2>
-            <img src="img/logo-meio.png" alt="">
-            <ul class="qualidade_lista">
-                <li class="grid-1-3"><h3>Durabilidade</h3>
-                    <p> Ainda assim, Existem Dúvidas a respeito de como a necessidade de renovação e inovação</p></li>
-                <li class="grid-1-3"><h3>Design</h3>
-                    <p> Ainda assim, Existem Dúvidas a respeito de como a necessidade de renovação e inovação</p></li>                                         <li class="grid-1-3"><h3>Sustentabilidade</h3>
-                    <p> Ainda assim, Existem Dúvidas a respeito de como a necessidade de renovação e inovação</p></li>
-            </ul>
-            <div class="call">
-                <p>Conheça Mais a Nossa História</p>
-                <a href="sobre.php" class="btn btn-preto">Sobre</a>
-            </div>
-        </section>
-        <!-- FIM SOBRE-->
-        <section class="quebra">
-            <blockquote class="quote-externo container">
-                <p>" Não tenha nada em sua cassa que você não considere útil ou acredita ser bonito"</p>
-                <cite>william morris</cite></blockquote>
-        </section>
-        <!-- Fim QUEBRA-->
-        <footer>
-            <div class="footer">
-                <div class="container">
-                    <div class="grid-8 footer_historia">
-                        <h3>nossa história</h3>
-                        <p>O segredo da vida não é nada mais que viver intensamente, cada momento, trazido por ele mesma. O Segredo da Felicidade está na Vida, Segredo da Vida é saber viver, o Segredo de viver é saber Amar, o Segredo de Amar é conhecer a Deus.</p>
-                    </div>
+    <li class="grid-1-3">
+     <div class="produtos_icone"> 
+      <img src="./img/produtos/retro.png" alt="[BykMin Passeio]" title="TiMídia Passeio"></div>
+     <h3>Retro</h3>
+     <p> Não tenha nada em sua cassa que você não considere útil ou acredita ser bonito</p>
+    </li>
+   </ul>
+   <div class="call">
+    <p>Clique Aqui e Veja os Detalhes dos Produtos</p>
+    <a href="Produtos.php" class="btn btn-preto">Produtos</a>
+   </div>
+  </section>
+  <!-- FECHA PRODUTOS --> 
+  <section class="portfolio">
+   <div class="container">
+    <h2 class="subtitulo">Portfólio</h2>
+    <div class="portfolio_lista">
+     <div class="grid-8"><img src="img/portfolio/retro.jpg" alt="[Bicicleta Retro]"></div>
+     <div class="grid-8"><img src="img/portfolio/passeio.jpg" alt="[Bicicleta Passeio]"></div> 
+     <div class="grid-16"><img src="img/portfolio/esporte.jpg" alt="[Bicicleta Esporte]"></div>
+    </div>
+    <div class="call">
+     <p>Conheça Mais Sobre Nosso Portfólio</p>
+     <a href="portfolio.php" class="btn">Portfólio</a>
+    </div>
+   </div>            
+  </section>            <!-- FIM PORTFÓLIO -->
+  <section class="qualidade container">
+   <h2 class="subtitulo">Qualidade</h2>
+   <img src="img/logo-meio.png" alt="">
+   <ul class="qualidade_lista">
+    <li class="grid-1-3"><h3>Durabilidade</h3>
+     <p> Ainda assim, Existem Dúvidas a respeito de como a necessidade de renovação e inovação</p></li>
+    <li class="grid-1-3"><h3>Design</h3>
+     <p> Ainda assim, Existem Dúvidas a respeito de como a necessidade de renovação e inovação</p></li>                                         <li class="grid-1-3"><h3>Sustentabilidade</h3>
+     <p> Ainda assim, Existem Dúvidas a respeito de como a necessidade de renovação e inovação</p></li>
+   </ul>
+   <div class="call">
+    <p>Conheça Mais a Nossa História</p>
+    <a href="sobre.php" class="btn btn-preto">Sobre</a>
+   </div>
+  </section>
+  <!-- FIM SOBRE-->
+  <section class="quebra">
+   <blockquote class="quote-externo container">
+    <p>" Não tenha nada em sua cassa que você não considere útil ou acredita ser bonito"</p>
+    <cite>william morris</cite></blockquote>
+  </section>
+  <!-- Fim QUEBRA-->
+  <footer>
+   <div class="footer">
+    <div class="container">
+     <div class="grid-8 footer_historia">
+      <h3>nossa história</h3>
+      <p>O segredo da vida não é nada mais que viver intensamente, cada momento, trazido por ele mesma. O Segredo da Felicidade está na Vida, Segredo da Vida é saber viver, o Segredo de viver é saber Amar, o Segredo de Amar é conhecer a Deus.</p>
+     </div>
 
-                    <div class="grid-4 footer_contato">
-                        <h3>CONTATO</h3>
-                        <ul>
-                            <li>- 64 9999999</li>
-                            <li>contato@gmail.com</li>
-                            <li>Cidade - ******* <b>UF</b></li>
-                        </ul>
-                    </div>
+     <div class="grid-4 footer_contato">
+      <h3>CONTATO</h3>
+      <ul>
+       <li>- 64 9999999</li>
+       <li>contato@gmail.com</li>
+       <li>Cidade - ******* <b>UF</b></li>
+      </ul>
+     </div>
 
-                    <div class="grid-4 footer_redes">
-                        <h3>Rede Sociais</h3>
-                        <ul>
-                            <li><a target="_blank" href="https://facebook.com"><img src="./img/redes-sociais/facebook.png"></a></li>
-
-
-                            <li><a target="_blank" href="https://www.instagram.com/"><img src="./img/redes-sociais/instagram.png"></a></li>
-
-                            <li><a target="_blank" href="https://twitter.com/"><img src="./img/redes-sociais/twitter.png"></a></li> 
+     <div class="grid-4 footer_redes">
+      <h3>Rede Sociais</h3>
+      <ul>
+       <li><a target="_blank" href="https://facebook.com"><img src="./img/redes-sociais/facebook.png"></a></li>
 
 
-                        </ul>
-                    </div>
+       <li><a target="_blank" href="https://www.instagram.com/"><img src="./img/redes-sociais/instagram.png"></a></li>
 
-                </div>
+       <li><a target="_blank" href="https://twitter.com/"><img src="./img/redes-sociais/twitter.png"></a></li> 
 
-            </div>
 
-            <div class="copy">
-                <div class="container">
-                    <p class="grid-16">  &copy 2015 - <?php echo date('Y'); ?> BykMin - Todos os direitos Reservados</p>
-                </div>
-            </div>
-        </footer> 
-        <!-- -->
-        <!-- -->
-        <!-- --> 
-        <!-- -->
-        <!-- -->
-        <!--  JavaScript-->
-        <script src="js/libs/jquery.min.js"></script>
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
-        <!-- Fecha JavaScript-->
-        <script>
+      </ul>
+     </div>
 
-        </body>
-        </html>
+    </div>
+
+   </div>
+
+   <div class="copy">
+    <div class="container">
+     <p class="grid-16">  &copy 2015 - <?php echo date('Y'); ?> BykMin - Todos os direitos Reservados</p>
+    </div>
+   </div>
+  </footer> 
+  <!-- -->
+  <!-- -->
+  <!-- --> 
+  <!-- -->
+  <!-- -->
+  <!--  JavaScript-->
+  <script src="js/libs/jquery.min.js"></script>
+  <script src="js/plugins.js"></script>
+  <script src="js/main.js"></script>
+  <!-- Fecha JavaScript-->
+  <script>
+
+  </body>
+  </html>
